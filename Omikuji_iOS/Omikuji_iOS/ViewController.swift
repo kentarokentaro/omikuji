13f2fd377523b968ddaf30c541c7e9635295392c
kentarokentaro <oikimutida@yahoo.co.jp>
2017-02-09 16:38:37 +0900

    modify layout

--- Omikuji_iOS/Omikuji_iOS/ViewController.swift
+++ Omikuji_iOS/Omikuji_iOS/ViewController.swift
@@ -22,7 +22,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /* レイアウトセット */
+
+        // 枠線色
+        fortuneLabel.layer.borderColor = UIColor.green.cgColor
+        // 枠線太さ
+        //fortuneLabel.layer.borderWidth = 0.5
+        // 枠線角度
+        //fortuneLabel.layer.cornerRadius = 50
+        // 枠線外を削る
+        fortuneLabel.layer.masksToBounds = true
+        // 枠線角度 : を丸にする
+        fortuneLabel.layer.cornerRadius = fortuneLabel.bounds.width / 2
     }
 
     override func didReceiveMemoryWarning() {
