9a87cafa3f5aa5e7cfc0d9675672b5210762b9d1
kentarokentaro <oikimutida@yahoo.co.jp>
2017-02-09 16:05:49 +0900

    set IBOutlet

--- Omikuji_iOS/Omikuji_iOS/ViewController.swift
+++ Omikuji_iOS/Omikuji_iOS/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fortuneLabel: UILabel!
+    
+    @IBAction func getFortune(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
